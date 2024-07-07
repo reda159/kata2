5f1ad5ca291019178e913f2d35c09239af330356
reda <reda.guirrine@ynov.com>
2024-07-07 12:05:57 +0200

    ex5: code - should handle different delimiters

--- calculator.js
+++ calculator.js
@@ -4,6 +4,22 @@ function add(numbers) {
     }
 
     let delimiter = ",";
+    let checkOtherDelimiter = "";
+    // Check for personalize delimiter
+    if (numbers.startsWith("//")) {
+        // Get the new delimiter and delete it from the initial strings
+        const delimiterEnd = numbers.indexOf("\n");
+        delimiter = numbers.substring(2, delimiterEnd);
+        numbers = numbers.substring(delimiterEnd + 1);
+        // To check for another delimiter we delete all the character that is either a numbers or the delimiter
+        checkOtherDelimiter = numbers.replace(/\d/g, "")
+        checkOtherDelimiter = checkOtherDelimiter.replace(new RegExp("[" + delimiter + "]", "g"), "")
+    }
+
+    // Check if there is invalid characters
+    if (checkOtherDelimiter.length >= 1) {
+        throw new Error("'" + delimiter + "' expected but '" + checkOtherDelimiter[0] + "' found at position " + numbers.indexOf(checkOtherDelimiter[0]) + ".");
+    }
 
     // Replace newlines with the delimiter
     numbers = numbers.replace(/\n/g, delimiter);

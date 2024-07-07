75e3c025102e48db9757313ae591a4943ca6103a
reda <reda.guirrine@ynov.com>
2024-07-07 11:41:05 +0200

    ex1: coma - should handle two numbers separated by comma

--- calculator.js
+++ calculator.js
@@ -3,6 +3,13 @@ function add(numbers) {
         return 0;
     }
 
+    if (numbers.includes(",")) {
+        const index = numbers.indexOf(",");
+        const num1 = parseInt(numbers.substring(0, index));
+        const num2 = parseInt(numbers.substring(index + 1));
+        return num1 + num2;
+    }
+
     return parseInt(numbers);
 }
 
